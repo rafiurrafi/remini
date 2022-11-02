a60710a1edb5c8975b558564417c5f0bef040cf1
Rafiur Rahman <rrfoundary@gmail.com>
2022-11-02 19:13:17 +0600

    added loading functionaltiy

--- src/quote-gen/quote-gen-app.jsx
+++ src/quote-gen/quote-gen-app.jsx
@@ -4,19 +4,24 @@ import UserInfo from "./user-info/user-info.component";
 
 const QuoteGenApp = () => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     generateUser();
   }, []);
   function generateUser() {
+    setLoading(true);
     const randomId = Math.floor(Math.random() * 9) + 1;
     fetch("https://jsonplaceholder.typicode.com/users/" + randomId)
       .then((response) => response.json())
-      .then((user) => setUser(user));
+      .then((user) => {
+        setUser(user);
+        setLoading(false);
+      });
   }
   return (
     <div>
       <h1>Quote gen app</h1>
-      {user ? <UserInfo user={user} /> : <h2>No user found</h2>}
+      {!loading ? <UserInfo user={user} /> : <h2>Loading...</h2>}
       <button onClick={generateUser}>Get another user</button>
     </div>
   );
