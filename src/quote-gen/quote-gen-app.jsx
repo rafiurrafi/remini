dff0e820cbe20131ae29a60544fbbc0d590eeb0d
Rafiur Rahman <rrfoundary@gmail.com>
2022-11-02 18:59:16 +0600

    added fetch user functionality

--- src/quote-gen/quote-gen-app.jsx
+++ src/quote-gen/quote-gen-app.jsx
@@ -1,7 +1,21 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const QuoteGenApp = () => {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    generateUser();
+  }, []);
+  function generateUser() {
+    const randomId = Math.floor(Math.random() * 10);
+    fetch("https://jsonplaceholder.typicode.com/users/" + randomId)
+      .then((response) => response.json())
+      .then((user) => setUser(user));
+  }
   return (
     <div>
       <h1>Quote gen app</h1>
+      {user ? <h1>User</h1> : <h2>No user found</h2>}
     </div>
   );
 };
