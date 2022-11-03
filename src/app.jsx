89a8dd324fe764a872adacd8e7f9caf1b6c3aa8f
Rafiur Rahman <rrfoundary@gmail.com>
2022-11-03 21:53:31 +0600

    added quiz app new project

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,8 @@ import { Provider } from "react-redux";
 import QuoteGenApp from "./quote-gen/quote-gen-app";
 import store from "./quote-gen/store/store";
 const App = () => {
+  // Working with api map
+  // https://www.youtube.com/playlist?list=PLWnON6N0wn-GBZi4cj_sz9grBtNZec3Tm
   return (
     <div>
       {/* <BudgetApp /> */}
---
+++ src/quiz/quiz-app.jsx
@@ -0,0 +1,9 @@
+const QuizApp = () => {
+  return (
+    <div>
+      <h1>Quiz app</h1>
+    </div>
+  );
+};
+
+export default QuizApp;
