0c377d4cd86b5808a62db3a0ea7c46345df9218c
Rafiur Rahman <rrfoundary@gmail.com>
2022-11-20 19:03:31 +0600

    add menu items to sidebar

--- src/dashboard/components/sidebar.jsx
+++ src/dashboard/components/sidebar.jsx
@@ -1,5 +1,24 @@
+import { Link } from "react-router-dom";
+import { links } from "../data/dummy";
+
 const Sidebar = () => {
-    return <h1>Sidebar</h1>;
-  };
-  
-  export default Sidebar;
+  return (
+    <div className="sidebar">
+      {links.map((item) => (
+        <>
+          <h4>{item.title}</h4>
+
+          {item.links.map((link) => (
+            <p>
+              <Link to={`/${link.name}`}>
+                {link.icon} {link.name.toUpperCase()}
+              </Link>
+            </p>
+          ))}
+        </>
+      ))}
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/dashboard/pages/charts/pyramid.jsx
@@ -0,0 +1,5 @@
+const Pyramid = () => {
+  return <h1>Pyramid</h1>;
+};
+
+export default Pyramid;
