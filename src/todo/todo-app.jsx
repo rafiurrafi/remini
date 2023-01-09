9f0f752daa958d80d18711163976eea0350442a5
Rafiur Rahman <rrfoundary@gmail.com>
2023-01-09 23:03:20 +0600

    added on enter press in input

--- src/todo/todo-app.jsx
+++ src/todo/todo-app.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+
 const TodoApp = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  function handleChange(e) {
+    setSearchQuery(e.target.value);
+  }
+  function handleKeyPress(e) {
+    if (e.key === "Enter") {
+      console.log("Done");
+    }
+  }
   return (
     <div>
       <h1>Todo</h1>
+      <input
+        type="text"
+        value={searchQuery}
+        onChange={handleChange}
+        onKeyDown={handleKeyPress}
+      />
     </div>
   );
 };
